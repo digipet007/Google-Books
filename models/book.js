49d2107460c0ea0799477f8e3d50286aabebe488
Sarah Arnold <digipet007@gmail.com>
2019-12-21 01:23:56 -0800

    added unique qualifier for title

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   author: [{ type: String, required: true }],
   description: String,
   date: { type: Date, default: Date.now },
